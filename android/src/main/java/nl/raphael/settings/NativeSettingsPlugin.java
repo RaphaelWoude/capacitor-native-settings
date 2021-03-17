b50d2ca6e72d5b01ca2340c757e2543d74dd1acf
Raphael van der Woude <47354694+RaphaelWoude@users.noreply.github.com>
2021-03-17 14:12:18 +0100

    Crash: ACTION_APPLICATION_DETAIL_SETTINGS (#5)
    * Fixed a bug were ACTION_APPLICATION_DETAIL_SETTINGS would crash on android
    * Fixed linting

--- android/src/main/java/nl/raphael/settings/NativeSettingsPlugin.java
+++ android/src/main/java/nl/raphael/settings/NativeSettingsPlugin.java
@@ -1,8 +1,9 @@
 package nl.raphael.settings;
 
+import static android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS;
+
 import android.content.Intent;
 import android.net.Uri;
-import android.os.Build;
 import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
@@ -17,12 +18,26 @@ public class NativeSettingsPlugin extends Plugin {
         String option = call.getString("option");
         String setting = AndroidSettings.getAction(option);
 
+        // Check if settings is available.
         if (setting == null) {
             call.reject("Could not find native android setting: " + option);
             return;
         }
 
-        getActivity().startActivity(new Intent(setting));
+        Intent intent = new Intent();
+
+        // Application details requires package name as URI.
+        if (ACTION_APPLICATION_DETAILS_SETTINGS.equals(setting)) {
+            intent.setAction(setting);
+            intent.setData(Uri.parse("package:" + getActivity().getPackageName()));
+        } else {
+            intent.setAction(setting);
+        }
+
+        // Start intent in activity.
+        getActivity().startActivity(intent);
+
+        // Send response.
         JSObject js = new JSObject();
         js.put("status", true);
         call.resolve(js);
