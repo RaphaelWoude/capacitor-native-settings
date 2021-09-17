72022d5fee41b5852700d29c34c3e1bef085e65c
Sebastian Paluchiewicz <sebastian.paluchiewicz@gmail.com>
2021-09-17 10:06:48 +0200

    iOS implementation updated to run on main thread.

--- ios/Plugin/NativeSettingsPlugin.swift
+++ ios/Plugin/NativeSettingsPlugin.swift
@@ -8,26 +8,33 @@ import Capacitor
 @objc(NativeSettingsPlugin)
 public class NativeSettingsPlugin: CAPPlugin {
     @objc func openIOS(_ call: CAPPluginCall) {
-        let value = call.getString("option") ?? ""
-        var settingsUrl:URL!
-        
-        if (value == "general") {
-            settingsUrl = URL(string: "App-Prefs:root=General")
-        } else if (value == "app") {
-            settingsUrl = URL(string: UIApplication.openSettingsURLString)
-        } else {
-            call.reject("Requested setting \"" + value + "\" is not available on iOS.");
+        guard let value = call.getString("option") else {
+            call.reject("Requested empty setting is not available on iOS.")
             return
         }
 
-        if UIApplication.shared.canOpenURL(settingsUrl) {
-            UIApplication.shared.open(settingsUrl, completionHandler: { (success) in
-                call.resolve([
-                    "status": success
-                ])
-            })
-        } else {
-            call.reject("Cannot open settings")
+        var settingsUrl: URL!
+
+        switch value {
+        case "general":
+            settingsUrl = URL(string: "App-Prefs:root=General")
+        case "app":
+            settingsUrl = URL(string: UIApplication.openSettingsURLString)
+        default:
+            call.reject("Requested setting \"" + value + "\" is not available on iOS.")
+            return
+        }
+
+        DispatchQueue.main.sync {
+            if UIApplication.shared.canOpenURL(settingsUrl) {
+                UIApplication.shared.open(settingsUrl, completionHandler: { (success) in
+                    call.resolve([
+                        "status": success
+                    ])
+                })
+            } else {
+                call.reject("Cannot open settings")
+            }
         }
     }
 }
--- ios/PluginTests/NativeSettingsPluginTests.swift
+++ ios/PluginTests/NativeSettingsPluginTests.swift
@@ -1,25 +1,24 @@
 import XCTest
+import Capacitor
 @testable import Plugin
 
 class NativeSettingsTests: XCTestCase {
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
 
-    func testEcho() {
-        // This is an example of a functional test case for a plugin.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-
-        let implementation = NativeSettings()
-        let value = "Hello, World!"
-        let result = implementation.echo(value)
-
-        XCTAssertEqual(value, result)
+    func test_pluginProvidesRequiredMethod() {
+        let sut = NativeSettingsPlugin()
+        sut.conforms(to: NativeSettingsPluginProtocol.self)
     }
 }
+
+@objc protocol NativeSettingsPluginProtocol {
+    @objc func openIOS(_ call: CAPPluginCall)
+}
+
+extension NativeSettingsPlugin: NativeSettingsPluginProtocol { }
