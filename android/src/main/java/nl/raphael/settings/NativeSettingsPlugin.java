96690009ded649248d1aa7f38a16b20806b2ad92
Jonathan Breidfjord <jbreidfjord@gmail.com>
2022-06-13 02:58:33 -0600

    Add AppNotification Option on Android (#20)
    * add AppNotification
    * add enum variant for app notification
    * add extra app package to intent

--- android/src/main/java/nl/raphael/settings/NativeSettingsPlugin.java
+++ android/src/main/java/nl/raphael/settings/NativeSettingsPlugin.java
@@ -1,6 +1,8 @@
 package nl.raphael.settings;
 
 import static android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS;
+import static android.provider.Settings.ACTION_APP_NOTIFICATION_SETTINGS;
+import static android.provider.Settings.EXTRA_APP_PACKAGE;
 
 import android.content.Intent;
 import android.net.Uri;
@@ -48,6 +50,9 @@ public class NativeSettingsPlugin extends Plugin {
         if (ACTION_APPLICATION_DETAILS_SETTINGS.equals(setting)) {
             intent.setAction(setting);
             intent.setData(Uri.parse("package:" + getActivity().getPackageName()));
+        } else if (ACTION_APP_NOTIFICATION_SETTINGS.equals(setting)) { // App notification settings requires package name as extra app package.
+            intent.setAction(setting);
+            intent.putExtra(EXTRA_APP_PACKAGE, getActivity().getPackageName());
         } else {
             intent.setAction(setting);
         }
